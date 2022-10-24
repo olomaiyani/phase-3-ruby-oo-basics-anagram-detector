66632f2c8e885cfccdadbf3958bf63b383e87426
Nana <nanaolomaiyani@gmail.com>
2022-10-24 22:42:51 +0300

    commit tree a373c521af1d684f7a1fc959d4dea0f41c23b4aa

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+    @word=word
+    end
+
+    def match(words)
+        match=[]
+        for word in words do
+            if (word.chars.sort.join ==@word.chars.sort.join)
+                match << word
+            end
+        end
+        match
+    end
+end
